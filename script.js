1f71afa813aa387f4a5dbacac7731885bd5ce617
TarkanV <carchaido@gmail.com>
2023-01-25 20:44:18 +0100

    Add player and computer choice inputs

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function getComputerChoice(){
+const choiceNbr = ["rock","paper", "scissors"];
 
+function getComputerChoice(){
+    choice = randomBetween(1, 3);
+    console.log(`The computer chose ${choiceNbr[choice - 1]}.`);
+    return choice;
+}
+function getPlayerChoice(){
+    let playerSelection = prompt("Write your choice of hand : ").toLowerCase();
+    console.log(`You chose ${playerSelection}.`);
+    for (let i=0; i < choiceNbr.length; i++) 
+        return (playerSelection == choiceNbr[i]) ? i+1 : "failed";  
+}
+
+function randomBetween(min, max){
+    return Math.floor(Math.random()*(max - min + 1) + min);
 }
 
 function play(playerSelection, computerSelection){
-    const playerSelection = ps = ps.charAt(0).toUpperCase() + ps.slice(1).toLowerCase();
+    
     const win = true;
     const outcome = (win) ? "win" : "lose";
     return `You ${outcome}!`;
@@ -11,7 +25,19 @@ function play(playerSelection, computerSelection){
 
 
 function game(){
-    for(let i = 0; i < 5; i++){
-        const computerSelection = getComputerChoice();
-    }   
+    newGame = true;
+    console.log("Welcome to Rock Paper Scissors Game!");
+    do{
+        for(let i = 0; i < 5; i++){
+            let playerScore = 0; 
+            let computerScore = 0;
+            const playerSelection = getPlayerChoice();
+            const computerSelection = getComputerChoice();
+            
+        }  
+    playAgainAnswer = prompt("Do you want to play again? (y/n)");
+    playAgain = (playAgainAnswer.toLowerCase() === "y") ? true : false;
+    }while(playAgain === true); 
 }
+
+game();
